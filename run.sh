00f642826ff362b08101e306c01e9c69e954071d
Chaitanya Akkineni <cakkineni@gmail.com>
2015-08-13 15:40:05 -0500

    Displaying log

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-echo ` awk 'NR==1 {print $1}' /etc/hosts` > /var/www/index.html
-lighttpd -D -f /etc/lighttpd/lighttpd.conf
+lighttpd -D -f /etc/lighttpd/lighttpd.conf &
+
+while true; do
+    sed 's/$/<br>/' /log  | tac > /var/www/index.html
+    sleep 5
+done
