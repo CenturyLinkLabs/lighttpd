a4525bc930c0a2dbffedf2b72132909e144e0194
Chaitanya Akkineni <cakkineni@gmail.com>
2015-08-14 08:22:43 -0500

    Displaying log

--- run.sh
+++ run.sh
@@ -3,16 +3,14 @@
 lighttpd -D -f /etc/lighttpd/lighttpd.conf &
 
 while true; do
-  echo
-    "<html>
+  echo "<html>
     <head>
         <title>Chimera install Log</title>
         <meta http-equiv="refresh" content="5">
     </head>
     <body>" > /var/www/index.html
   tail -50 /log | tac | sed 's/$/<br>/' >> /var/www/index.html
- echo
-    "</body>
+ echo "</body>
     </html>" >> /var/www/index.html
     sleep 5
 done
